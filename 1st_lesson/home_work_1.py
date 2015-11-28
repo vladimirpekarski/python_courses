d883ab9993fa5a62443daf3a9476791348dd67e9
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-11-28 09:43:45 +0300

    Fix After Review

--- 1st_lesson/home_work_1.py
+++ 1st_lesson/home_work_1.py
@@ -2,23 +2,24 @@ __author__ = 'vladimir.pekarsky'
 
 import random
 
-def generate_random_list(length):
+def generate_random_list(length=20):
     random_list = []
     for i in range(length):
         random_list.append(random.randint(0, 500))
 
     return random_list
 
-def count_odd_index_sum(list_length=20):
+def count_odd_index_sum(test_list):
     s = 0
-    random_list = generate_random_list(list_length)
-    if not random_list:
+    if not test_list:
         return s
 
-    for index, value in enumerate(random_list):
+    for index, value in enumerate(test_list):
         if index % 2 != 0:
             s += value
 
-    return s * random_list[-1]
+    return s * test_list[-1]
 
-print(count_odd_index_sum())
+print(count_odd_index_sum([0, 1, 2, 3, 4, 5]))
+print(count_odd_index_sum([]))
+print(count_odd_index_sum(generate_random_list()))
