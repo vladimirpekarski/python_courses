993ff355c0b81aae95842d4cca1b3afee8d27c98
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-12-04 22:23:48 +0300

    Fix after review

--- 2nd_lesson/currency.py
+++ 2nd_lesson/currency.py
@@ -4,23 +4,33 @@ import json
 import sys
 import urllib2
 
-init_request = ("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange"
-                "%20where%20pair%20%3D%20%22USDBYR%2CEURBYR%2CRUBBYR%22&format=json&env="
-                "store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+
+def get_request(*pairs_list):
+    request_first_part = ("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange"
+                          "%20where%20pair%20%3D%20%22")
+    request_last_part = "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    pairs = '%2C'.join(pairs_list)
+
+    return request_first_part + pairs + request_last_part
 
 
 def get_currency_rate(*currency_list):
+    if not currency_list[0]:
+        return 'Please provide pairs'
+
     data = {}
-    rate_list = get_rate_list(init_request)
-    for currency in currency_list:
-        for rate in rate_list:
-            if currency == rate['id']:
-                data[rate['Name']] = float(rate['Rate'])
+    request = get_request(*currency_list)
+    rates = get_rates(request)
+    if isinstance(rates, dict):
+        data[rates['Name']] = float(rates['Rate'])
+    elif isinstance(rates, list):
+        for rate in rates:
+            data[rate['Name']] = float(rate['Rate'])
 
     return data
 
 
-def get_rate_list(request):
+def get_rates(request):
     response = urllib2.urlopen(request)
     check_response_code(response)
     response_str = response.read()
@@ -37,4 +47,6 @@ def check_response_code(response):
 
 print(get_currency_rate('USDBYR', 'EURBYR', 'RUBBYR'))
 print(get_currency_rate('USDBYR', 'EURBYR'))
+print(get_currency_rate('USDEUR', 'EURUSD'))
 print(get_currency_rate('USDBYR'))
+print(get_currency_rate([]))
