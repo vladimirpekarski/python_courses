ea9a6874071b66be5b6c40b9070dfa783484a6b2
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-12-14 23:05:15 +0300

    fiinal version, plus pdf with graph

--- 6th_lesson/home_work/compare_sorting.py
+++ 6th_lesson/home_work/compare_sorting.py
@@ -1,77 +1,95 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vladimir.pekarsky'
+
+import argparse
 import random
 import time
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Comparing standard python sort method versus bubble sort')
+    parser.add_argument('integers', metavar='N', type=int, nargs=5,
+                        help='Provide 5 integers value for quantities.')
+
+    return parser.parse_args()
+
 def generate_random_list(length):
-    print('INFO: Start generate list')
     random_list = []
     for _ in xrange(length):
         random_list.append(random.randrange(0, length))
 
-    print('INFO: Finish generate list')
-    # print('INFO: random list', random_list)
     return random_list
 
 
 def sort_bubble(list_to_sort):
-    print('INFO: Start bubble sort')
     for i in range(len(list_to_sort) - 1):
         for j in range(len(list_to_sort) - i - 1):
             if list_to_sort[j] > list_to_sort[j + 1]:
                 temp = list_to_sort[j]
                 list_to_sort[j] = list_to_sort[j + 1]
                 list_to_sort[j + 1] = temp
-    print('INFO: Finish bubble sort')
-    # print('INFO: sorted list by bubble', list_to_sort)
+
 
 def standard_search(list_to_sort):
-    print('INFO: Start standard sort')
     list_to_sort.sort()
-    print('INFO: Finish standard sort')
-    # print('INFO: sorted list by standard', list_to_sort)
 
 
 def get_execution_time(function, quantity_of_elements):
-    print('INFO: Start test')
     test_list = generate_random_list(quantity_of_elements)
     start_time = time.time()
     function(test_list)
     finish_time = time.time()
     delta = finish_time - start_time
-    print('INFO: Finish test')
-    print('=' * 50)
 
     return delta
 
 
 def get_average_time(temp_results):
-    print('INFO: Start finding average')
     temp_results.remove(max(temp_results))
     temp_results.remove(min(temp_results))
     average = sum(temp_results) / len(temp_results)
-    print('INFO: Finish finding average')
 
     return average
 
 
 def run_test(function, quantity_of_elements):
     results = []
-
     for _ in xrange(5):
         results.append(get_execution_time(function,
                                           quantity_of_elements))
-
     average = get_average_time(results)
+    print('...Test for {}({}) has finished in {} sec'.
+          format(function.__name__, quantity_of_elements, average))
 
     return average
 
 
-# print(run_test(sort_bubble, 50000))
-print(get_execution_time(sort_bubble, 50000))
-# print('Standard search {} ms'.format(get_execution_time(standard_search,
-#                                                         100)))
+def run_package_test(function, _list_of_elements):
+    results = []
+    for _quantity in _list_of_elements:
+        results.append([_quantity, run_test(function, _quantity)])
+
+    return results
 
 
+def print_results(results):
+    for result in results:
+        print('[{}]: {} sec'.format(result[0], result[1]))
+
+
+if __name__ == '__main__':
+    list_of_elements = parse_args().integers
+    print('Bubbles sort test:')
+    bubble_results = run_package_test(sort_bubble, list_of_elements)
+
+    print('Standard sort test:')
+    standard_results = run_package_test(standard_search,
+                                        list_of_elements)
+
+    print('Bubble sort results:')
+    print_results(bubble_results)
+
+    print('Standard sort results:')
+    print_results(standard_results)
 
